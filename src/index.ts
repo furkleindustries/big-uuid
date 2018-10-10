b4a44b70fd31637f75880de8f8e852ae889c0127
furkle <furkle@furkleindustries.com>
2018-10-10 18:35:16 -0400

    New version.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
 /* An RFC 4112 UUID generator for generating IFIDs, or Interactive Fiction
  * Identifiers. */
 
-export * from './Enums/AGTVersions';
-export * from './Enums/IFIDVersions';
-export * from './Enums/MagneticScrollsDocumentedTitles';
-export * from './Enums/NamespaceIds';
-
-export * from './IFID/IFID';
-export * from './IFID/IIFID';
-export * from './IFID/IIFIDOptions';
-
 export * from './UUID/UUIDOptions/IUUIDOptions';
 export * from './UUID/UUIDOptions/UUIDOptions';
 export * from './UUID/IUUID';
