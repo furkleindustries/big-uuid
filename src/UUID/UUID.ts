82144ee51c6a889519bc06456d1175c1bf613626
furkle <furkle@furkleindustries.com>
2018-11-08 20:58:55 -0500

    Fixing vulns in dependencies.

--- src/UUID/UUID.ts
+++ src/UUID/UUID.ts
@@ -167,7 +167,6 @@ export class UUID implements IUUID {
     const version = parseInt(this.version).toString(2);
     const timeHighNum = uintArrayAsNumber(timeHigh).toString(2);
     const binStr = version.padStart(4, '0') + timeHighNum.padStart(12, '0');
-    console.log(binStr);
     const firstByte = parseInt(binStr.slice(0, 8), 2);
     const secondByte = parseInt(binStr.slice(8, 16), 2);
     return new Uint8Array([ firstByte, secondByte, ]);
